e03bde728cc43adfec947dd4f59379ad25eb12ab
Erick Namukolo <erickmndev@gmail.com>
2025-07-25 03:32:53 +0200

    added add website screen

--- lib/features/websites/screens/add_website_screen.dart
+++ lib/features/websites/screens/add_website_screen.dart
@@ -1,14 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:pulse/widgets/custom_appbar.dart';
+import 'package:pulse/widgets/custom_button.dart';
+import 'package:pulse/widgets/custom_text_field.dart';
 
 class AddWebsiteScreen extends StatelessWidget {
   const AddWebsiteScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       appBar: CustomAppBar(
         title: 'Add Website',
+      ),
+      body: Padding(
+        padding: EdgeInsets.all(15.0),
+        child: Column(
+          children: [
+            CustomTextField(hint: 'Website Name'),
+            CustomTextField(hint: 'Website URL'),
+            const Spacer(),
+            CustomButton(btnText: 'Save', click: () {}),
+          ],
+        ),
       ),
     );
   }
